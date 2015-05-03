f4b92fc18ee7eb86675a5f0e3dc11832b0f4c054
unknown <jpatrickhall@gmail.com>
2015-05-03 13:05:13 -0400

    some simplifications

--- run_on_chunks.py
+++ run_on_chunks.py
@@ -1,29 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-DISCLAIMER:  
-                                                                
-THIS INFORMATION IS PROVIDED "AS IS". THERE ARE NO WARRANTIES,
-EXPRESSED OR IMPLIED, AS TO MERCHANTABILITY OR FITNESS FOR A
-PARTICULAR PURPOSE REGARDING THE ACCURACY OF THE MATERIALS OR CODE
-CONTAINED HEREIN.
+Copyright (c) 2015 by Patrick Hall, jpatrickhall@gmail.com
 
-jpatrickhall@gmail.com
-12.06.14
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-Stubs with simple examples for using Python multiprocessing to complete 
-independent tasks. 
+   http://www.apache.org/licenses/LICENSE-2.0
 
-N_THREAD: Number of processes to use; the script will create this many 
-          chunks and working sub-directores. 
-         
-WORKING_DIR: Parent directory in which the sub-directories for each 
-             chunk will be created. 
-             
-INPUT_DATA: File to split into chunks and process. 
-        
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+-------------------------------------------------------------------------------
+
+Stub with simple example for using Python multiprocessing to complete
+independent tasks.
+
+N_THREAD: Number of processes to use; the script will create this many
+          chunks and working sub-directores.
+
+WORKING_DIR: Parent directory in which the sub-directories for each
+             chunk will be created.
+
+INPUT_DATA: File to split into chunks and process.
+
 Run by setting constants here OR by command line:
 run_on_chunks.py -f <input file> -p <number of processes> -d <output directory>
-            
+
 """
 
 import getopt
@@ -34,17 +40,17 @@ import sys
 import time
 from multiprocessing import Process
 
-# TODO: Set constants 
-N_THREAD = 2
-WORKING_DIR = '/tmp'
+# TODO: Set constants
+N_THREAD = 4
+WORKING_DIR = 'C:\Temp'
 INPUT_DATA = 'sample_data.txt'
 
 def create_out_dirs():
 
-    """ Creates N_THREAD number of output directories. 
-    
+    """ Creates N_THREAD number of output directories.
+
     Raises:
-        EnvironemtError: Problem creating directories. 
+        EnvironemtError: Problem creating directories.
     """
 
     print '-------------------------------------------------------------------'
@@ -100,7 +106,7 @@ def chunk_files():
 
         print 'Done.'
 
-def task_1(i, in_prefix='_raw'):
+def some_task(i, in_prefix='_raw'):
 
     """ Some task that executes on each chunk independently.
 
@@ -113,72 +119,17 @@ def task_1(i, in_prefix='_raw'):
 
     chunk_dir = WORKING_DIR + os.sep + '_chunk_dir' + str(i)
     in_file = chunk_dir + os.sep + in_prefix + str(i) + '.txt'
-    out_file = chunk_dir + os.sep + in_prefix + '_task_1_completed' +\
+    out_file = chunk_dir + os.sep + in_prefix + '_some_task_completed' +\
         str(i)  + '.txt'
 
     process_name = multiprocessing.current_process().name
-    print process_name + ': initialized ... '
 
     out = open(out_file, 'wb')
     with open(in_file) as in_f:
+        # TODO: your task here
         for j, line in enumerate(in_f):
-            # TODO: your task here
-            if j % 10 == 0:
-                print process_name + ': ' + str(j)
-            out.write(line)
-
-def task_2(i, in_prefix='_raw_task_1_completed'):
-
-    """ The next sample task that executes on each chunk independently.
-
-    Args:
-        i: Thread index.
-        in_prefix: The filename prefix for locating input files; can be
-            changed to alter the order of execution.
-
-    """
-
-    chunk_dir = WORKING_DIR + os.sep + '_chunk_dir' + str(i)
-    in_file = chunk_dir + os.sep + in_prefix + str(i) + '.txt'
-    out_file = chunk_dir + os.sep + in_prefix + '_task_2_completed' +\
-        str(i)  + '.txt'
-
-    process_name = multiprocessing.current_process().name
-    print process_name + ': initialized ... '
-
-    out = open(out_file, 'wb')
-    with open(in_file) as in_f:
-        for j, line in enumerate(in_f):
-            # TODO: your task here
-            if j % 10 == 0:
-                print process_name + ': ' + str(j)
-            out.write(line)
-
-def task_3(i, in_prefix='_raw_task_1_completed_task_2_completed'):
-
-    """ Just one more sample task that executes on each chunk independently.
-
-    Args:
-        i: Thread index.
-        in_prefix: The filename prefix for locating input files; can be
-            changed to alter the order of execution.
-
-    """
-
-    chunk_dir = WORKING_DIR + os.sep + '_chunk_dir' + str(i)
-    in_file = chunk_dir + os.sep + in_prefix + str(i) + '.txt'
-    out_file = chunk_dir + os.sep + in_prefix + '_task_3_completed' +\
-        str(i)  + '.txt'
-
-    process_name = multiprocessing.current_process().name
-    print process_name + ': initialized ... '
-
-    out = open(out_file, 'wb')
-    with open(in_file) as in_f:
-        for j, line in enumerate(in_f):
-            # TODO: your task here
-            if j % 10 == 0:
-                print process_name + ': ' + str(j)
+            if j % 100 == 0:
+                print process_name + ': Hello world ...'
             out.write(line)
 
 def main(argv):
@@ -192,11 +143,11 @@ def main(argv):
         BaseException: Some problem from a task.
     """
 
-    # Init vars 
+    # Init vars
     n_thread = None
     input_data = None
     working_dir = None
-    
+
     global N_THREAD
     global INPUT_DATA
     global WORKING_DIR
@@ -232,8 +183,6 @@ def main(argv):
     print 'Output directory (-d)    = %s' % (WORKING_DIR)
     print 'Input file (-f)          = %s' % (INPUT_DATA)
 
-    big_tic = time.time()
-
     # Init chunk directory structure and chunks
     create_out_dirs()
     chunk_files()
@@ -242,63 +191,23 @@ def main(argv):
     # You may have as many tasks as you like
 
     print '-------------------------------------------------------------------'
-    print 'Executing task 1 on chunks ... '
+    print 'Executing some task on chunks ... '
     tic = time.time()
     processes = []
     try:
         for i in range(0, int(N_THREAD)):
             process_name = 'Process_' + str(i)
-            process = Process(target=task_1, name=process_name, args=(i,))
+            process = Process(target=some_task, name=process_name, args=(i,))
             process.start()
             processes.append(process)
         for process_ in processes:
             process_.join()
-        print 'Completed task 1 in %.2f s.' % (time.time()-tic)
+        print 'Completed some task in %.2f s.' % (time.time()-tic)
     except BaseException as exception_:
         print exception_.message
         print 'ERROR: Could not complete task 1.'
         print sys.exc_info()
         exit(-1)
-
-    print '-------------------------------------------------------------------'
-    print 'Executing task 2 on chunks ... '
-    tic = time.time()
-    processes = []
-    try:
-        for i in range(0, int(N_THREAD)):
-            process_name = 'Process_' + str(i)
-            process = Process(target=task_2, name=process_name, args=(i,))
-            process.start()
-            processes.append(process)
-        for process_ in processes:
-            process_.join()
-        print 'Completed task 2 in %.2f s.' % (time.time()-tic)
-    except BaseException as exception_:
-        print exception_.message
-        print 'ERROR: Could not complete task 2.'
-        print sys.exc_info()
-        exit(-1)
-
-    print '-------------------------------------------------------------------'
-    print 'Executing task 3 on chunks ... '
-    tic = time.time()
-    processes = []
-    try:
-        for i in range(0, int(N_THREAD)):
-            process_name = 'Process_' + str(i)
-            process = Process(target=task_3, name=process_name, args=(i,))
-            process.start()
-            processes.append(process)
-        for process_ in processes:
-            process_.join()
-        print 'Completed task 3 in %.2f s.' % (time.time()-tic)
-    except BaseException as exception_:
-        print exception_.message
-        print 'ERROR: Could not complete task 3.'
-        print sys.exc_info()
-        exit(-1)
-
-    print 'Completed all tasks in %.2f s.' % (time.time()-big_tic)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
